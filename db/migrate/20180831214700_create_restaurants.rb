5e6e29fcaeeb786509236320bd3d1be088b56c24
Jordan Rand <jorrand@gmail.com>
2018-08-31 15:52:36 -0600

    init

---
+++ db/migrate/20180831214700_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.float :rating
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
